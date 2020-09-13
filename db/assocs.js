4c8119e9716963dce9669cc6c70a093b913f757c
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2020-09-13 23:34:46 +0200

    add sequelize assocs

---
+++ db/assocs.js
@@ -0,0 +1,7 @@
+function createAssociations(sequelize) {
+  //const { instrument, orchestra } = sequelize.models;
+  //orchestra.hasMany(instrument);
+  //instrument.belongsTo(orchestra);
+}
+
+module.exports = { createAssociations };
--- db/index.js
+++ db/index.js
@@ -5,23 +5,21 @@ const config = require("../config");
 
 const sequelize = new Sequelize(config.DB_URL);
 
-function applyConnections(sequelize) {
-  //const { instrument, orchestra } = sequelize.models;
-  //orchestra.hasMany(instrument);
-  //instrument.belongsTo(orchestra);
-}
+const { createAssociations } = require("./assocs");
 
-/*const modelDefiners = [
-	require('./models/node.model'),
-	require('./models/recipe.model'),
+const modelDefiners = [
+  require("./models/node.model"),
+  require("./models/recipe.model"),
+  require("./models/user.model"),
+  require("./models/material.model"),
 ];
 
 // We define all models according to their files.
 for (const modelDefiner of modelDefiners) {
-	modelDefiner(sequelize);
-}*/
+  modelDefiner(sequelize);
+}
 
-applyExtraSetup(sequelize);
+createAssociations(sequelize);
 
 // We export the sequelize connection instance to be used around our app.
 module.exports = sequelize;
