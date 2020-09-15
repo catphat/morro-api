e27db37d600d5624314ebca1995af568a2abdcbf
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2020-09-15 22:47:30 +0200

    provide favicon

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ router.get("/", (req, res) => {
   res.send("Hello, from morrolan.tv api. Have a good day!");
 });
 
-router.get("/favicon.ico", (req, res) => {
-  res.sendStatus(204);
-});
-
 router.use("/nodes", require("./nodes.route"));
 router.use("/recipes", require("./recipes.route"));
 router.use("/user", require("./user.route"));
