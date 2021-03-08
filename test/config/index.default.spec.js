658a388303c762a393a249eaece0aeaf1c59e1c3
ishqbb <ishqsquad@gmail.com>
2021-03-08 12:24:15 -0700

    change test case for eu base-url

--- test/config/index.default.spec.js
+++ test/config/index.default.spec.js
@@ -10,7 +10,7 @@ const {describe, it} = require("mocha");
 // this test case is intentionally excessive and not an indicator of expected test coverage. (ishq)
 describe('default env', function() {
    it('should have correct default values', () => {
-      expect(config.MARKET_BASE_URL_EU).to.equal('NOT IMPLEMENTED');
+      expect(config.MARKET_BASE_URL_EU).to.equal('https://eu-trade.naeu.playblackdesert.com');
       expect(config.MARKET_BASE_URL_NA).to.equal('https://na-trade.naeu.playblackdesert.com');
       expect(config.MARKET_TOKEN_EU).to.be.undefined;
       expect(config.MARKET_TOKEN_NA).to.be.undefined;
