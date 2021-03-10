4ffae8ac04cd2c81888ee86850e54bd6b5db27bb
ishqbb <ishqsquad@gmail.com>
2021-03-10 13:59:43 -0700

    format test/config with eslint #4

--- test/config/index.default.spec.js
+++ test/config/index.default.spec.js
@@ -1,31 +1,31 @@
-'use strict';
-
 const path = require('path');
+
 const configPath = path.resolve('src/config');
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
+
 const config = require(configPath);
-const {describe, it} = require("mocha");
+const { describe, it } = require('mocha');
 
 // this test case is intentionally excessive and not an indicator of expected test coverage. (ishq)
-describe('default env', function() {
-   it('should have correct default values', () => {
-      expect(config.MARKET_BASE_URL_EU).to.equal('https://eu-trade.naeu.playblackdesert.com');
-      expect(config.MARKET_BASE_URL_NA).to.equal('https://na-trade.naeu.playblackdesert.com');
-      expect(config.MARKET_TOKEN_EU).to.be.undefined;
-      expect(config.MARKET_TOKEN_NA).to.be.undefined;
-      expect(config.DB_URL).to.be.undefined;
-      expect(config.PORT).to.be.undefined;
-      expect(config.ENV).to.equal('development');
-      expect(config.CACHE_LIFETIME_MIN).to.equal(60);
-      expect(config.MARKETPLACE_REQUEST_DELAY_MS).to.equal(50);
-      expect(config.GEAR_EMAIL).to.be.undefined;
-      expect(config.AUTH_AUDIENCE).to.equal("https://api.morrolan.tv");
-      expect(config.SE_TOKEN).to.be.undefined;
-      expect(config.SE_BASEURL).to.equal("https://api.streamelements.com/kappa/v2/");
-      expect(config.SE_CHANNEL_ID).to.equal("595fe4c2ff8f5f74ac7fcfbc");
-      expect(config.TWITCH_CLIENT_ID).to.be.undefined;
-      expect(config.TWITCH_CLIENT_SECRET).to.be.undefined;
-      expect(config.TWITCH_TOKEN_SCOPES).to.equal("chat:read+bits:read+channel:read:subscriptions")
-   })
-})
+describe('default env', () => {
+  it('should have correct default values', () => {
+    expect(config.MARKET_BASE_URL_EU).to.equal('https://eu-trade.naeu.playblackdesert.com');
+    expect(config.MARKET_BASE_URL_NA).to.equal('https://na-trade.naeu.playblackdesert.com');
+    expect(config.MARKET_TOKEN_EU).to.be.undefined;
+    expect(config.MARKET_TOKEN_NA).to.be.undefined;
+    expect(config.DB_URL).to.be.undefined;
+    expect(config.PORT).to.be.undefined;
+    expect(config.ENV).to.equal('development');
+    expect(config.CACHE_LIFETIME_MIN).to.equal(60);
+    expect(config.MARKETPLACE_REQUEST_DELAY_MS).to.equal(50);
+    expect(config.GEAR_EMAIL).to.be.undefined;
+    expect(config.AUTH_AUDIENCE).to.equal('https://api.morrolan.tv');
+    expect(config.SE_TOKEN).to.be.undefined;
+    expect(config.SE_BASEURL).to.equal('https://api.streamelements.com/kappa/v2/');
+    expect(config.SE_CHANNEL_ID).to.equal('595fe4c2ff8f5f74ac7fcfbc');
+    expect(config.TWITCH_CLIENT_ID).to.be.undefined;
+    expect(config.TWITCH_CLIENT_SECRET).to.be.undefined;
+    expect(config.TWITCH_TOKEN_SCOPES).to.equal('chat:read+bits:read+channel:read:subscriptions');
+  });
+});
--- test/config/index.dotenv.spec.js
+++ test/config/index.dotenv.spec.js
@@ -1,28 +1,28 @@
-'use strict';
-
 const path = require('path');
+
 const configPath = path.resolve('src/config');
 const dotEnvPath = path.resolve('test/test.env');
 
-require('dotenv').config({path: dotEnvPath})
-const expect = require('chai').expect;
+require('dotenv').config({ path: dotEnvPath });
+const { expect } = require('chai');
+
 const config = require(configPath);
-const {describe, it} = require("mocha");
+const { describe, it } = require('mocha');
 
 // this test case is intentionally excessive and not an indicator of expected test coverage. (ishq)
-describe('config', function() {
-   it('should have correct dotenv values', () => {
-      expect(config.MARKET_BASE_URL_EU).to.equal('127.0.0.1:9000');
-      expect(config.MARKET_BASE_URL_NA).to.equal('127.0.0.1:8000');
-      expect(config.MARKET_TOKEN_EU).to.equal('EUABC123')
-      expect(config.MARKET_TOKEN_NA).to.equal('NAABC123')
-      expect(config.DB_URL).to.equal("postgres://user:pass@example.com:5432/dbname")
-      expect(config.PORT).to.equal("7000");
-      expect(config.ENV).to.equal("TEST-DEV");
-      expect(config.GEAR_EMAIL).to.equal("efzUFE83suBCR5Z@FcqM9nGPLYb.com");
-      expect(config.SE_TOKEN).to.equal("fD3JT8CGdNA");
-      expect(config.TWITCH_CLIENT_ID).to.equal("VSoAt4J8paZ");
-      expect(config.TWITCH_CLIENT_SECRET).equal("ZhAzwXtyf7r");
-      expect(config.TWITCH_USER_ID).equal("4ZHC28bsaHX");
-   })
-})
+describe('config', () => {
+  it('should have correct dotenv values', () => {
+    expect(config.MARKET_BASE_URL_EU).to.equal('127.0.0.1:9000');
+    expect(config.MARKET_BASE_URL_NA).to.equal('127.0.0.1:8000');
+    expect(config.MARKET_TOKEN_EU).to.equal('EUABC123');
+    expect(config.MARKET_TOKEN_NA).to.equal('NAABC123');
+    expect(config.DB_URL).to.equal('postgres://user:pass@example.com:5432/dbname');
+    expect(config.PORT).to.equal('7000');
+    expect(config.ENV).to.equal('TEST-DEV');
+    expect(config.GEAR_EMAIL).to.equal('efzUFE83suBCR5Z@FcqM9nGPLYb.com');
+    expect(config.SE_TOKEN).to.equal('fD3JT8CGdNA');
+    expect(config.TWITCH_CLIENT_ID).to.equal('VSoAt4J8paZ');
+    expect(config.TWITCH_CLIENT_SECRET).equal('ZhAzwXtyf7r');
+    expect(config.TWITCH_USER_ID).equal('4ZHC28bsaHX');
+  });
+});
