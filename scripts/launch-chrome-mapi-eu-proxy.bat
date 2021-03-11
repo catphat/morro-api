d76ecaa95d46ceefeda6cc781f1f298485ebbe96
ishqbb <ishqsquad@gmail.com>
2021-03-10 17:20:28 -0700

    change localhost to 127.0.0.1

--- scripts/launch-chrome-mapi-eu-proxy.bat
+++ scripts/launch-chrome-mapi-eu-proxy.bat
@@ -1,3 +1,3 @@
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" ^
     --user-data-dir="%USERPROFILE%\mapi-eu-proxy-profile" ^
-    --proxy-server="socks5://localhost:7771"
+    --proxy-server="socks5://127.0.0.1:7771"
--- scripts/launch-chrome-mapi-na-proxy.bat
+++ scripts/launch-chrome-mapi-na-proxy.bat
@@ -1,3 +1,3 @@
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" ^
     --user-data-dir="%USERPROFILE%\mapi-na-proxy-profile" ^
-    --proxy-server="socks5://localhost:7770"
+    --proxy-server="socks5://127.0.0.1:7770"
