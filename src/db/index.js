ac0cc66b538589625dbec94431f257b2bab72fc9
ishqbb <ishqsquad@gmail.com>
2021-03-18 15:05:07 -0600

    clean up src/db/index #4

--- src/db/index.js
+++ src/db/index.js
@@ -1,40 +1,47 @@
-//Connect to database for materials, nodes etc.
-const { Sequelize } = require("sequelize");
+// Connect to database for materials, nodes etc.
+const { Sequelize } = require('sequelize');
 
-const config = require("../config");
+const config = require('../config');
 
 const sequelize = new Sequelize(config.DB_URL, {
   logging: false,
 });
 
-const { createAssociations } = require("./associations/assocs");
+const { createAssociations } = require('./associations/assocs');
+
+const nodeModel = require('./models/node.model');
+const recipeModel = require('./models/recipe.model');
+const materialModel = require('./models/material.model');
+const materialLogModel = require('./models/materiallog.model');
+const groupModel = require('./models/group.model');
+const userModel = require('./models/user.model');
+const userNodeModel = require('./models/usernode.model');
+const gearModel = require('./models/gear.model');
+const tokenModel = require('./models/token.model');
+
+const recipeIngredientAssoc = require('./associations/recipeIngredient.model');
+const recipeProductAssoc = require('./associations/recipeProduct.model');
+const nodeMaterialAssoc = require('./associations/nodeMaterial.model');
 
 const modelDefiners = [
-  require("./models/node.model"),
-  require("./models/recipe.model"),
-  require("./models/material.model"),
-  require("./models/materiallog.model"),
-  require("./models/group.model"),
-  require("./models/user.model"),
-  require("./models/usernode.model"),
-  require("./models/gear.model"),
-  require("./models/token.model"),
+  nodeModel,
+  recipeModel,
+  materialModel,
+  materialLogModel,
+  groupModel,
+  userModel,
+  userNodeModel,
+  gearModel,
+  tokenModel,
 ];
 
-// We define all models according to their files.
-for (const modelDefiner of modelDefiners) {
-  modelDefiner(sequelize);
-}
-
-// Create complex assocs with assoc models
 const assocDefiners = [
-  require("./associations/recipeIngredient.model"),
-  require("./associations/recipeProduct.model"),
-  require("./associations/nodeMaterial.model"),
+  recipeIngredientAssoc,
+  recipeProductAssoc,
+  nodeMaterialAssoc,
 ];
-for (const assocDefiner of assocDefiners) {
-  assocDefiner(sequelize);
-}
+modelDefiners.forEach((x) => x(sequelize));
+assocDefiners.forEach((x) => x(sequelize));
 
 // Create simple assocs with sequalize methods
 createAssociations(sequelize);
