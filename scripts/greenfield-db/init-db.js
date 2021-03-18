370cd38337baf368a6ea5a02daaeb6c9c30d54a5
ishqbb <ishqsquad@gmail.com>
2021-03-18 15:17:53 -0600

    clean up greenfield init script, move to script subdir.

--- scripts/greenfield-db/init-db.js
+++ scripts/greenfield-db/init-db.js
@@ -1,7 +1,6 @@
-"use strict";
+require('dotenv').config();
+const sequelize = require('../../src/db');
 
-require("dotenv").config();
-const sequelize = require("../src/db");
 const {
   Node,
   NodeMaterial,
@@ -11,21 +10,23 @@ const {
   RecipeIngredient,
   RecipeProduct,
 } = sequelize.models;
-const nodeList = require("./data/nodeList.json");
-const nodeGroups = require("./data/defaultGroups.json");
-const groupList = require("./data/itemGroups.json");
-const recipeList = require("./data/recipesList.json");
+
+const nodeList = require('./data/nodeList.json');
+const nodeGroups = require('./data/defaultGroups.json');
+const groupList = require('./data/itemGroups.json');
+const recipeList = require('./data/recipesList.json');
+
 const groupMap = new Map(nodeGroups);
 
 async function setNodes() {
-  //Use this script only in dev!
+  // Use this script only in dev!
   await Node.sync({ force: true });
   await NodeMaterial.sync({ force: true });
   await Promise.all(
     nodeList.map(async (node, index) => {
-      //Index + 1, database cant have 0 as PK
+      // Index + 1, database cant have 0 as PK
       await createNode(node, index + 1);
-    })
+    }),
   );
 }
 
@@ -47,11 +48,11 @@ async function createNode(node, index) {
       group:
         groupMap.has(index) && groupMap.get(index)
           ? groupMap
-              .get(index)
-              .replace(
-                /(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g,
-                '$1"$3":'
-              )
+            .get(index)
+            .replace(
+              /(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g,
+              '$1"$3":',
+            )
           : null,
     });
     for (const material of node.material) {
@@ -75,12 +76,12 @@ async function createNode(node, index) {
 }
 
 async function setGroups() {
-  //Use this script only in dev!
+  // Use this script only in dev!
   await Group.sync({ force: true });
   await Promise.all(
     groupList.map(async (group) => {
       await createGroup(group);
-    })
+    }),
   );
 }
 
@@ -103,15 +104,15 @@ async function createGroup(group) {
 }
 
 async function setRecipes() {
-  //Use this script only in dev!
+  // Use this script only in dev!
   await Recipe.sync({ force: true });
   await RecipeIngredient.sync({ force: true });
   await RecipeProduct.sync({ force: true });
   await Promise.all(
     recipeList.map(async (recipe) => {
-      //Index + 1, database cant have 0 as PK
+      // Index + 1, database cant have 0 as PK
       await createRecipe(recipe);
-    })
+    }),
   );
 }
 
@@ -144,16 +145,16 @@ async function createRecipe(recipe) {
 }
 
 async function main() {
-  var t0 = new Date().getTime();
+  const t0 = new Date().getTime();
   await setNodes();
   await setGroups();
   await setRecipes();
   await sequelize.close();
-  var t1 = new Date().getTime();
+  const t1 = new Date().getTime();
   console.log(`Initial import done! It took ${(t1 - t0) / 1000} seconds`);
 }
 
-//This script should be used local, then copy database
-if (process.env.NODE_ENV != "production") {
+// This script should be used local, then copy database
+if (process.env.NODE_ENV !== 'production') {
   main();
 }
