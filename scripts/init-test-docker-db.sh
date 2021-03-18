28214560de684682423b32fc0c2e40d1a18bf7c9
ishqbb <ishqsquad@gmail.com>
2021-03-18 16:43:11 -0600

    update docker script to include db name

--- scripts/init-test-docker-db.sh
+++ scripts/init-test-docker-db.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name mapi-db -e POSTGRES_PASSWORD=abc123 -p 5432:5432 -d postgres
+docker run --name docker-mapi-db -e POSTGRES_PASSWORD=abc123 -e POSTGRES_DB=mapi-db -p 5432:5432 -d postgres
