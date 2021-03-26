d400f9a6c30fd0c74fdee63e5e2c33dd0b5420f7
ishqbb <ishqsquad@gmail.com>
2021-03-25 18:14:58 -0600

    update curl-bdoclient-GetMarketPriceInfo.sh to allow item main-key args

--- scripts/curl-bdoclient-GetMarketPriceInfo.sh
+++ scripts/curl-bdoclient-GetMarketPriceInfo.sh
@@ -5,16 +5,24 @@
 # keyType - REQUIRED
 # mainKey - REQUIRED
 # subKey  - REQUIRED
+
+MAIN_KEY=$1
+
+if [[ -z "$MAIN_KEY" ]]; then
+  echo "item id (mainKey) must be provided."
+  exit 1
+fi
+
 curl "https://na-trade.naeu.playblackdesert.com/Trademarket/GetMarketPriceInfo" \
   -H 'Content-Type: application/json' \
   -H 'User-Agent: BlackDesert' \
   --socks5 "127.0.0.1:7770" \
   -X POST \
-  -d '{
-  "keyType": 0,
-  "mainKey": 5952,
-  "subKey": 0
-}'
+  -d "{
+  'keyType': 0,
+  'mainKey': $MAIN_KEY,
+  'subKey': 0
+}"
 
 # Response (flooded)
 # Sorted by date in asc order. First value being oldest, last value being most recent.
@@ -22,4 +30,3 @@ curl "https://na-trade.naeu.playblackdesert.com/Trademarket/GetMarketPriceInfo" 
 #   "resultCode":0,
 #   "resultMsg":"6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600-6600"
 # }
-
