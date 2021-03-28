e47feeb6b8c3febdd4fd90d5e7eb7419825f4fe6
ishqbb <ishqsquad@gmail.com>
2021-03-28 15:24:36 -0600

    add validation isPositiveNumber and associated tests #1

---
+++ src/validation/isPositiveNumber.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} max - max positive number allowed
+ * @return {function(*=): (null|string|string)} Returns null if value is a positive number.
+ *          And does not exceed max value if max is provided, otherwise returns error.
+ *          Ignores value if it's empty, use isRequired to enforce requirement.
+ */
+const isPositiveNumber = (max) => (value) => {
+  if (value === null
+    || value === undefined
+    || (typeof value === 'number'
+      && (Number.isNaN(value) || (value > 0 && (!max || (max && !(value > max))))))) {
+    return null;
+  }
+
+  if (max) {
+    return `Expected a positive number no greater than ${max}`;
+  }
+  return 'Expected a positive number';
+};
+
+module.exports = isPositiveNumber;
