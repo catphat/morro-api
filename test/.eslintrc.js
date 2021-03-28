5f9bac1256bbefba64a0dce7703414228456c51c
ishqbb <ishqsquad@gmail.com>
2021-03-27 20:43:11 -0600

    add test eslint

---
+++ test/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ['mocha'],
+  env: {
+    mocha: true,
+  },
+  rules: {
+    'padded-blocks': 0,
+    'no-unused-expressions': 0,
+    'no-unused-vars': 1,
+    'import/no-extraneous-dependencies': 0,
+  },
+};
