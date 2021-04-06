703f6ee84a0af94fea1c6bb83326b1a972f71d78
ishqbb <ishqsquad@gmail.com>
2021-04-06 15:49:15 -0600

    refactor getWorldMarketList to use updated transport, update respective tests #1

--- src/api/bdo_client/getWorldMarketList.js
+++ src/api/bdo_client/getWorldMarketList.js
@@ -1,114 +1,94 @@
-require('dotenv').config();
-const MarketUtil = require('./marketUtil');
+const { getRegionalBdoTransport } = require('./regionalBdoTransport');
+const { validateFields } = require('../../validation');
 
-class GetWorldMarketList {
-  constructor(region) {
-    this.mu = new MarketUtil(region);
-    this.client = this.mu.getClient(region);
-  }
+/**
+ *  @typedef {Object} RawWorldMarketListData
+ *  @property {number} resultCode - 0 indicates success
+ *  @property {string} resultMsg - Item data delimited by a '|'.
+ *                                 Each item is delimited by a '-'.
+ *                                 i.e.: ItemId-ItemStock-TotalTrades-BasePrice|ItemId-...
+ */
 
-  /**
-   * @param {!number} mainCategory
-   * @param {number} subCategory
-   * @param {number} keyType
-   */
-  getRequestParameters(mainCategory, subCategory = 0, keyType = 0) {
-    const paramType = typeof (1);
-    this.mu.throwIfParamUndefinedOrInvalidType('mainCategory', mainCategory, paramType);
-    if (keyType !== 0) {
-      this.mu.throwIfParamUndefinedOrInvalidType('keyType', keyType, paramType);
-    }
-    if (subCategory !== 0) {
-      this.mu.throwIfParamUndefinedOrInvalidType('subCategory', subCategory, paramType);
-    }
+/**
+ *  @typedef {Object} WorldMarketListItem
+ *  @property {number} itemId
+ *  @property {number} itemStock
+ *  @property {number} totalTrades
+ *  @property {number} basePrice
+ */
 
-    const body = {
-      keyType,
-      mainCategory,
-      subCategory,
-    };
+/**
+ *  @typedef {Object} ParsedWorldMarketList
+ *  @property {!boolean} isValid - received a valid resultCode (0)
+ *  @property {string | undefined} errorMsg - remote API provided error message if exists.
+ *  @property {!number} resultCode - remote API provided resultCode
+ *  @property {Array<WorldMarketListItem> | undefined} itemList
+ */
+
+/**
+ * @param {RawWorldMarketListData} resp
+ * @return {ParsedWorldMarketList}
+ */
+const parseResponse = (resp) => {
+  if (resp.resultCode !== 0) {
     return {
-      url: this.mu.getUrlByByRelativeUrl('Trademarket/GetWorldMarketList'),
-      opt: this.mu.getRequestOptions('POST', body),
+      isValid: false,
+      resultCode: resp.resultCode,
+      errorMsg: resp.resultMsg,
     };
   }
-
-  /**
-   *  @typedef {Object} WorldMarketListItem
-   *  @property {number} itemId
-   *  @property {number} itemStock
-   *  @property {number} totalTrades
-   *  @property {number} basePrice
-   */
-
-  /**
-   *  @typedef {Object} ParsedWorldMarketList
-   *  @property {!boolean} isValid - received a valid resultCode (0)
-   *  @property {string | undefined} errorMsg - remote API provided error message if exists.
-   *  @property {!number} resultCode - remote API provided resultCode
-   *  @property {Array<WorldMarketListItem> | undefined} itemList
-   */
-
-  /**
-   * @param {RawWorldMarketListData} resp
-   * @return {ParsedWorldMarketList}
-   */
-  static handleResponse(resp) {
-    if (resp.resultCode !== 0) {
-      return {
-        isValid: false,
-        resultCode: resp.resultCode,
-        errorMsg: resp.resultMsg,
-      };
+  const itemList = resp.resultMsg.split('|').reduce((result, x) => {
+    const entry = x.split('-');
+    const itemId = parseInt(entry[0], 10);
+    if (itemId === null || Number.isNaN(itemId)) {
+      // trailing delimiter '|' can lead to a bunch of NaN/Null property values
+      return result;
     }
-    const itemList = resp.resultMsg.split('|').map((x) => {
-      const entry = x.split('-');
-      const itemId = parseInt(entry[0], 10);
-      const itemStock = parseInt(entry[1], 10);
-      const totalTrades = parseInt(entry[2], 10);
-      const basePrice = parseInt(entry[3], 10);
-      return {
-        itemId,
-        itemStock,
-        totalTrades,
-        basePrice,
-      };
+    const itemStock = parseInt(entry[1], 10);
+    const totalTrades = parseInt(entry[2], 10);
+    const basePrice = parseInt(entry[3], 10);
+    result.push({
+      itemId,
+      itemStock,
+      totalTrades,
+      basePrice,
     });
 
-    return {
-      isValid: true,
-      resultCode: resp.resultCode,
-      itemList,
-    };
+    return result;
+  }, []);
+
+  return {
+    isValid: true,
+    resultCode: resp.resultCode,
+    itemList,
+  };
+};
+
+const validation = {
+  mainCategory: ['isRequired', 'isPositiveNumber'],
+  subCategory: ['isRequired', 'isPositiveNumber'],
+  keyType: ['isPositiveNumber'],
+};
+
+const getWorldMarketList = (region) => async ({
+  mainCategory,
+  subCategory,
+  keyType,
+}) => {
+  const payload = {
+    mainCategory,
+    subCategory,
+    keyType,
+  };
+  if (payload.keyType === undefined) {
+    payload.keyType = 0;
   }
+  validateFields(payload, validation);
 
-  /**
-   *  @typedef {Object} RawWorldMarketListData
-   *  @property {number} resultCode - 0 indicates success
-   *  @property {string} resultMsg - Item data delimited by a '|'.
-   *                                 Each item is delimited by a '-'.
-   *                                 i.e.: ItemId-ItemStock-TotalTrades-BasePrice|ItemId-...
-   */
+  const path = 'GetWorldMarketList';
+  const { post } = getRegionalBdoTransport(region);
+  const resp = await post(path, payload);
+  return parseResponse(resp);
+};
 
-  /**
-   * @param {!number} mainCategory
-   * @param {number} subCategory
-   * @param {number} keyType
-   * @return {Promise<RawWorldMarketListData>}
-   */
-  async byMainCategorySubCategoryAndKeyType(mainCategory, subCategory = 0, keyType = 0) {
-    const params = this.getRequestParameters(mainCategory, subCategory, keyType);
-    try {
-      const resp = await this.client.post(
-        params.url.href, params.opt.body, { headers: params.opt.headers },
-      );
-      this.mu.throwIfInvalidHTTPStatusCodeError(resp.status, resp.data);
-
-      return resp.data;
-    } catch (error) {
-      return Promise.reject(new Error(error));
-    }
-  }
-}
-
-module.exports = GetWorldMarketList;
+module.exports = { getWorldMarketList };
--- test/unit/api/bdo_client/getWorldMarketList.spec.js
+++ test/unit/api/bdo_client/getWorldMarketList.spec.js
@@ -1,63 +1,17 @@
-const {
-  getRawResponse, describe, expect, it, nock,
-} = require('../../../helpers/bdo_client');
+const doTest = require('../../../helpers/bdoClientHandlerTest');
 
-const { GetWorldMarketList } = require('../../../../src/api/bdo_client/index');
+describe('bdo_client/getWorldMarketList', () => {
+  const config = {
+    responseFile: 'worldmarketlist-1_1_0.json',
+    expectedFile: 'worldmarketlist-expected-parse-1_1_0.json',
+    handler: 'getWorldMarketList',
+    params: { mainCategory: 11607, subCategory: 1, keyType: 0 },
+    validation: {
+      mainCategory: ['isRequired', 'isPositiveNumber'],
+      subCategory: ['isRequired', 'isPositiveNumber'],
+      keyType: ['isPositiveNumber'],
+    },
+  };
 
-describe('GetWorldMarketList method', () => {
-  const worldMarketList = new GetWorldMarketList('NA');
-  const responseJSON = getRawResponse('worldmarketlist-1_1_0.json');
-  const invalidResponseTXT = getRawResponse('invalid-response.txt');
-  const reqParams = worldMarketList.getRequestParameters(1, 1, 0);
-
-  it('has valid request parameters', () => {
-    const req = worldMarketList.getRequestParameters(1, 1, 0);
-    expect(req.url.href).to.equal('http://127.0.0.1:8000/Trademarket/GetWorldMarketList');
-    expect(req.opt.method).to.equal('POST');
-    expect(req.opt.body).to.equal('{"keyType":0,"mainCategory":1,"subCategory":1}');
-  });
-
-  it('byKeyTypeMainAndSubCategory has valid handling of req/resp', async () => {
-    nock(`http://${reqParams.url.host}`)
-      .post(reqParams.url.pathname)
-      // eslint-disable-next-line func-names
-      .reply((uri, requestBody) => {
-        expect(uri).to.equal('/Trademarket/GetWorldMarketList');
-        expect(requestBody.mainCategory).to.equal(1);
-        expect(requestBody.subCategory).to.equal(1);
-        expect(requestBody.keyType).to.equal(0);
-
-        return [
-          200,
-          responseJSON,
-          { 'Content-Type': 'application/json' }];
-      });
-
-    const resp = await new GetWorldMarketList('NA').byMainCategorySubCategoryAndKeyType(1, 1, 0);
-    const parsedResp = GetWorldMarketList.handleResponse(resp);
-    expect(resp.resultCode).to.equal(0);
-    expect(parsedResp.isValid).to.be.true;
-    expect(parsedResp.itemList[0].itemId).to.equal(10003);
-    expect(parsedResp.itemList[0].itemStock).to.equal(9);
-    expect(parsedResp.itemList[0].totalTrades).to.equal(57278);
-    expect(parsedResp.itemList[0].basePrice).to.equal(17300);
-    expect(parsedResp.errorMsg).to.be.undefined;
-  });
-
-  it('byMainKey throws on invalid HTTP status code', async () => {
-    nock(`http://${reqParams.url.host}`)
-      .post(reqParams.url.pathname)
-      // eslint-disable-next-line func-names
-      .reply((uri, requestBody) => {
-        expect(uri).to.equal('/Trademarket/GetWorldMarketList');
-        expect(requestBody.mainCategory).to.equal(1);
-        expect(requestBody.subCategory).to.equal(1);
-        expect(requestBody.keyType).to.equal(0);
-
-        return [
-          444, invalidResponseTXT];
-      });
-
-    await expect(new GetWorldMarketList('NA').byMainCategorySubCategoryAndKeyType(1, 1, 0)).to.be.rejectedWith(Error);
-  });
+  doTest(config);
 });
