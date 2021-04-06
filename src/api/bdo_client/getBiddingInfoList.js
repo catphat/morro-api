a5e43a3cad5c2025cde5f51ea924d970c2a52d1b
ishqbb <ishqsquad@gmail.com>
2021-04-06 16:45:52 -0600

    refactor getBiddingInfoList to use updated transport, update respective tests #1

--- src/api/bdo_client/getBiddingInfoList.js
+++ src/api/bdo_client/getBiddingInfoList.js
@@ -1,100 +1,94 @@
-require('dotenv').config();
-const MarketUtil = require('./marketUtil');
+const { getRegionalBdoTransport } = require('./regionalBdoTransport');
+const { validateFields } = require('../../validation');
 
-class GetBiddingInfoList {
-  constructor(region) {
-    this.mu = new MarketUtil(region);
-    this.client = this.mu.getClient(region);
-  }
+/**
+ *  @typedef {Object} RawBiddingInfoListData
+ *  @property {number} resultCode - 0 indicates success
+ *  @property {string} resultMsg - Buy/Sell orders delimited by a '|'.
+ *                                 Each order item being delimited by a '-'.
+ *                                 i.e.: Price-SellOrders-BuyOrders|Price-SellOrders-BuyOrders...
+ */
 
-  /**
-   * @param {number} mainKey
-   */
-  getRequestParameters(mainKey) {
-    const body = {
-      keyType: 0,
-      mainKey,
-      subKey: 0,
-    };
+/**
+ *  @typedef {Object} BiddingInfoItem
+ *  @property {number} Price
+ *  @property {number} SellOrders
+ *  @property {number} BuyOrders
+ */
 
-    // noinspection SpellCheckingInspection
+/**
+ *  @typedef {Object} ParsedBiddingInfoList
+ *  @property {!boolean} isValid - received a valid resultCode (0)
+ *  @property {string | undefined} errorMsg - remote API provided error message if exists.
+ *  @property {!number} resultCode - remote API provided resultCode
+ *  @property {Array<BiddingInfoItem>|undefined} biddingList - all orders for item
+ */
+
+/**
+ * @param {RawBiddingInfoListData} resp
+ */
+const parseResponse = (resp) => {
+  if (resp.resultCode !== 0) {
     return {
-      url: this.mu.getUrlByByRelativeUrl('Trademarket/GetBiddingInfoList'),
-      opt: this.mu.getRequestOptions('POST', body),
-    };
-  }
-
-  /**
-   *  @typedef {Object} BiddingInfoItem
-   *  @property {number} Price
-   *  @property {number} SellOrders
-   *  @property {number} BuyOrders
-   */
-
-  /**
-   *  @typedef {Object} ParsedBiddingInfoList
-   *  @property {!boolean} isValid - received a valid resultCode (0)
-   *  @property {string | undefined} errorMsg - remote API provided error message if exists.
-   *  @property {!number} resultCode - remote API provided resultCode
-   *  @property {Array<BiddingInfoItem>|undefined} biddingList - all orders for item
-   */
-
-  /**
-   * @param {RawBiddingInfoListData} resp
-   */
-  static handleResponse(resp) {
-    if (resp.resultCode !== 0) {
-      return {
-        isValid: false,
-        resultCode: resp.resultCode,
-        errorMsg: resp.resultMsg,
-      };
-    }
-    const biddingList = resp.resultMsg.split('|').map((x) => {
-      const entry = x.split('-');
-      const price = parseInt(entry[0], 10);
-      const sellOrders = parseInt(entry[1], 10);
-      const buyOrders = parseInt(entry[2], 10);
-      return {
-        price,
-        sellOrders,
-        buyOrders,
-      };
-    });
-
-    return {
-      isValid: true,
+      isValid: false,
       resultCode: resp.resultCode,
-      biddingList,
+      errorMsg: resp.resultMsg,
     };
   }
-
-  /**
-   *  @typedef {Object} RawBiddingInfoListData
-   *  @property {number} resultCode - 0 indicates success
-   *  @property {string} resultMsg - Buy/Sell orders delimited by a '|'.
-   *                                 Each order item being delimited by a '-'.
-   *                                 i.e.: Price-SellOrders-BuyOrders|Price-SellOrders-BuyOrders...
-   */
-
-  /**
-   * Get current buy/sell orders for an item.
-   * @param {int} mainKey
-   * @return {Promise<RawBiddingInfoListData>}
-   */
-  async byMainKey(mainKey) {
-    const params = this.getRequestParameters(mainKey);
-    try {
-      const resp = await this.client.post(
-        params.url.href, params.opt.body, { headers: params.opt.headers },
-      );
-      this.mu.throwIfInvalidHTTPStatusCodeError(resp.status, resp.data);
-
-      return resp.data;
-    } catch (error) {
-      return Promise.reject(new Error(error));
+  const biddingList = resp.resultMsg.split('|').reduce((result, x) => {
+    const entry = x.split('-');
+    const price = parseInt(entry[0], 10);
+    if (price === null || Number.isNaN(price)) {
+      // trailing delimiter '|' can lead to a bunch of NaN/Null property values
+      return result;
     }
-  }
-}
+    const sellOrders = parseInt(entry[1], 10);
+    const buyOrders = parseInt(entry[2], 10);
+    result.push({
+      price,
+      sellOrders,
+      buyOrders,
+    });
+    return result;
+  }, []);
 
-module.exports = GetBiddingInfoList;
+  return {
+    isValid: true,
+    resultCode: resp.resultCode,
+    biddingList,
+  };
+};
+
+const validation = {
+  mainKey: ['isRequired', 'isPositiveNumber'],
+  subKey: ['isPositiveNumber'],
+  keyType: ['isPositiveNumber'],
+};
+
+const getBiddingInfoList = (region) => async ({
+  mainKey,
+  keyType,
+  subKey,
+}) => {
+  const payload = {
+    mainKey,
+    keyType,
+    subKey,
+  };
+
+  if (payload.keyType === undefined) {
+    payload.keyType = 0;
+  }
+
+  if (payload.subKey === undefined) {
+    payload.subKey = 0;
+  }
+
+  validateFields(payload, validation);
+  const path = 'GetBiddingInfoList';
+  const { post } = getRegionalBdoTransport(region);
+  const resp = await post(path, payload);
+  return parseResponse(resp);
+};
+
+module.exports = { getBiddingInfoList };
--- test/unit/api/bdo_client/getBiddingInfoList.spec.js
+++ test/unit/api/bdo_client/getBiddingInfoList.spec.js
@@ -1,64 +1,17 @@
-const {
-  getRawResponse, describe, expect, it, nock,
-} = require('../../../helpers/bdo_client');
+const doTest = require('../../../helpers/bdoClientHandlerTest');
 
-const { GetBiddingInfoList } = require('../../../../src/api/bdo_client/index');
+describe('bdo_client/getBiddingInfoList', () => {
+  const config = {
+    responseFile: 'biddinginfolist-11629.json',
+    expectedFile: 'biddinginfolist-expected-parse-11629.json',
+    handler: 'getBiddingInfoList',
+    params: { mainKey: 11629, subKey: 0, keyType: 0 },
+    validation: {
+      mainKey: ['isRequired', 'isPositiveNumber'],
+      subKey: ['isPositiveNumber'],
+      keyType: ['isPositiveNumber'],
+    },
+  };
 
-describe('BiddingInfoList method', () => {
-  it('has valid parameters', () => {
-    const biddingInfoList = new GetBiddingInfoList('NA');
-    const req = biddingInfoList.getRequestParameters(11629);
-    expect(req.url.href).to.equal('http://127.0.0.1:8000/Trademarket/GetBiddingInfoList');
-    expect(req.opt.method).to.equal('POST');
-    expect(req.opt.body).to.equal('{"keyType":0,"mainKey":11629,"subKey":0}');
-  });
-
-  describe('BiddingInfoList', () => {
-    const marketPriceInfo = new GetBiddingInfoList('NA');
-    const responseJSON = getRawResponse('biddinginfolist-11629.json');
-    const invalidResponseTXT = getRawResponse('invalid-response.txt');
-    const reqParams = marketPriceInfo.getRequestParameters(11607);
-
-    it('byMainKey has valid handling of req/resp', async () => {
-      nock(`http://${reqParams.url.host}`)
-        .post(reqParams.url.pathname)
-      // eslint-disable-next-line func-names
-        .reply((uri, requestBody) => {
-          expect(uri).to.equal('/Trademarket/GetBiddingInfoList');
-          expect(requestBody.keyType).to.equal(0);
-          expect(requestBody.mainKey).to.equal(11607);
-          expect(requestBody.subKey).to.equal(0);
-
-          return [
-            200,
-            responseJSON,
-            { 'Content-Type': 'application/json' }];
-        });
-
-      const resp = await new GetBiddingInfoList('NA').byMainKey(11607);
-      const parsedResp = GetBiddingInfoList.handleResponse(resp);
-      expect(resp.resultCode).to.equal(0);
-      expect(parsedResp.biddingList[0].price).to.equal(97500000);
-      expect(parsedResp.biddingList[0].sellOrders).to.equal(1);
-      expect(parsedResp.biddingList[0].buyOrders).to.equal(0);
-      expect(parsedResp.errorMsg).to.be.undefined;
-    });
-
-    it('byMainKey throws on invalid HTTP status code', async () => {
-      nock(`http://${reqParams.url.host}`)
-        .post(reqParams.url.pathname)
-      // eslint-disable-next-line func-names
-        .reply((uri, requestBody) => {
-          expect(uri).to.equal('/Trademarket/GetBiddingInfoList');
-          expect(requestBody.keyType).to.equal(0);
-          expect(requestBody.mainKey).to.equal(9999);
-          expect(requestBody.subKey).to.equal(0);
-
-          return [
-            444, invalidResponseTXT];
-        });
-
-      await expect(new GetBiddingInfoList('NA').byMainKey(9999)).to.be.rejectedWith(Error);
-    });
-  });
+  doTest(config);
 });
