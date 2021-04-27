dbd692b31a68f6662c930e0c4aa2f028ab0fa50b
catphat <kian@vedos.io>
2021-04-27 13:33:30 -0600

    remove unused marketUtils reference

--- src/api/bdo_client/index.js
+++ src/api/bdo_client/index.js
@@ -1,9 +1,8 @@
-const MarketUtil = require('./marketUtil');
 const GetMarketPriceInfo = require('./getMarketPriceInfo');
 const GetBiddingInfoList = require('./getBiddingInfoList');
 const GetWorldMarketList = require('./getWorldMarketList');
 const GetWorldMarketSubList = require('./getWorldMarketSubList');
 
 module.exports = {
-  MarketUtil, GetMarketPriceInfo, GetBiddingInfoList, GetWorldMarketList, GetWorldMarketSubList,
+  GetMarketPriceInfo, GetBiddingInfoList, GetWorldMarketList, GetWorldMarketSubList,
 };
