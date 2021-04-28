1cc4ba7f17df796916d62662a801ed3eb9d37226
catphat <kian@vedos.io>
2021-04-28 12:05:10 -0600

    clean up unit test index.dotenv.spec.js

--- test/unit/config/index.dotenv.spec.js
+++ test/unit/config/index.dotenv.spec.js
@@ -1,6 +1,50 @@
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+const searchCache = (moduleName, callback) => {
+  // Resolve the module identified by the specified name
+  let mod = require.resolve(moduleName);
+
+  // Check if the module has been resolved and found within
+  // the cache
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    // Recursively go over the results
+    (function traverse(mod) {
+      // Go over each of the module's children and
+      // traverse them
+      mod.children.forEach((child) => {
+        traverse(child);
+      });
+
+      // Call the specified callback providing the
+      // found cached module
+      callback(mod);
+    }(mod));
+  }
+};
+
+// require() caches, so the config module must be purged between tests
+// https://stackoverflow.com/a/14801711
+const purgeCache = (moduleName) => {
+  // Traverse the cache looking for the files
+  // loaded by the specified module name
+  searchCache(moduleName, (mod) => {
+    delete require.cache[mod.id];
+  });
+
+  // Remove cached paths to the module.
+  // Thanks to @bentael for pointing this out.
+  Object.keys(module.constructor._pathCache).forEach((cacheKey) => {
+    if (cacheKey.indexOf(moduleName) > 0) {
+      delete module.constructor._pathCache[cacheKey];
+    }
+  });
+};
+
 describe('config', () => {
 
   context('should have correct dotenv values when NODE_ENV is ', () => {
@@ -59,47 +103,3 @@ describe('config', () => {
 
   });
 });
-
-// require() caches, so the config module must be purged between tests
-// https://stackoverflow.com/a/14801711
-const purgeCache = (moduleName) => {
-  // Traverse the cache looking for the files
-  // loaded by the specified module name
-  searchCache(moduleName, (mod) => {
-    delete require.cache[mod.id];
-  });
-
-  // Remove cached paths to the module.
-  // Thanks to @bentael for pointing this out.
-  Object.keys(module.constructor._pathCache).forEach((cacheKey) => {
-    if (cacheKey.indexOf(moduleName) > 0) {
-      delete module.constructor._pathCache[cacheKey];
-    }
-  });
-};
-
-/**
- * Traverses the cache to search for all the cached
- * files of the specified module name
- */
-function searchCache(moduleName, callback) {
-  // Resolve the module identified by the specified name
-  let mod = require.resolve(moduleName);
-
-  // Check if the module has been resolved and found within
-  // the cache
-  if (mod && ((mod = require.cache[mod]) !== undefined)) {
-    // Recursively go over the results
-    (function traverse(mod) {
-      // Go over each of the module's children and
-      // traverse them
-      mod.children.forEach((child) => {
-        traverse(child);
-      });
-
-      // Call the specified callback providing the
-      // found cached module
-      callback(mod);
-    }(mod));
-  }
-}
