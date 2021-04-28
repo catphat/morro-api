d9a0554dc4ce19d6578a17ea3194345c037642a6
catphat <kian@vedos.io>
2021-04-27 21:14:50 -0600

    test config based on environment and increase associated test coverage #15 #20

--- test/unit/config/index.dotenv.spec.js
+++ test/unit/config/index.dotenv.spec.js
@@ -1,14 +1,95 @@
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
-const config = require('../../../src/config');
 
+const env = { ...process.env };
 describe('config', () => {
-  it('should have correct dotenv values', () => {
-    expect(config.ENV).to.equal('test');
-    expect(config.bdoClient.BASE_URL_NA).to.equal('http://127.0.0.1:9990');
-    expect(config.bdoClient.BASE_URL_EU).to.equal('http://127.0.0.1:9991');
-    expect(config.bdoClient.PROXY_URL).to.equal('127.0.0.1');
-    expect(config.bdoClient.PROXY_PORT).to.equal('7770');
-    expect(config.bdoClient.USE_PROXY).to.equal(true);
+
+  context('should have correct dotenv values when NODE_ENV is ', () => {
+    afterEach(() => {
+      process.env = env;
+      purgeCache('../../../src/config');
+    });
+
+    it('test', () => {
+      process.env.NODE_ENV = 'test';
+
+      const config = require('../../../src/config');
+      expect(config.ENV).to.equal('test');
+      expect(config.bdoClient.BASE_URL_NA).to.equal('http://127.0.0.1:9990');
+      expect(config.bdoClient.BASE_URL_EU).to.equal('http://127.0.0.1:9991');
+      expect(config.bdoClient.PROXY_URL).to.equal('127.0.0.1');
+      expect(config.bdoClient.PROXY_PORT).to.equal('7770');
+      expect(config.bdoClient.USE_PROXY).to.equal(true);
+    });
+
+    it('development', () => {
+      process.env.NODE_ENV = 'development';
+
+      const config = require('../../../src/config');
+      expect(config.ENV).to.equal('development');
+      expect(config.bdoClient.BASE_URL_NA).to.equal('https://na-trade.naeu.playblackdesert.com/Trademarket');
+      expect(config.bdoClient.BASE_URL_EU).to.equal('https://eu-trade.naeu.playblackdesert.com/Trademarket');
+      expect(config.bdoClient.PROXY_URL).to.equal('127.0.0.1');
+      expect(config.bdoClient.PROXY_PORT).to.equal('9990');
+      expect(config.bdoClient.USE_PROXY).to.equal(true);
+    });
+
+    it('production', () => {
+      process.env.NODE_ENV = 'production';
+
+      delete require.cache['../../../src/config'];
+      const config = require('../../../src/config');
+      expect(config.ENV).to.equal('production');
+      expect(config.bdoClient.BASE_URL_NA).to.equal('https://na-trade.naeu.playblackdesert.com/Trademarket');
+      expect(config.bdoClient.BASE_URL_EU).to.equal('https://eu-trade.naeu.playblackdesert.com/Trademarket');
+      expect(config.bdoClient.PROXY_URL).to.equal('127.0.0.1');
+      expect(config.bdoClient.PROXY_PORT).to.equal('1110');
+      expect(config.bdoClient.USE_PROXY).to.equal(true);
+    });
+
   });
 });
+
+// require() caches, so the config module must be purged between tests
+// https://stackoverflow.com/a/14801711
+const purgeCache = (moduleName) => {
+  // Traverse the cache looking for the files
+  // loaded by the specified module name
+  searchCache(moduleName, (mod) => {
+    delete require.cache[mod.id];
+  });
+
+  // Remove cached paths to the module.
+  // Thanks to @bentael for pointing this out.
+  Object.keys(module.constructor._pathCache).forEach((cacheKey) => {
+    if (cacheKey.indexOf(moduleName) > 0) {
+      delete module.constructor._pathCache[cacheKey];
+    }
+  });
+};
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache(moduleName, callback) {
+  // Resolve the module identified by the specified name
+  let mod = require.resolve(moduleName);
+
+  // Check if the module has been resolved and found within
+  // the cache
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    // Recursively go over the results
+    (function traverse(mod) {
+      // Go over each of the module's children and
+      // traverse them
+      mod.children.forEach((child) => {
+        traverse(child);
+      });
+
+      // Call the specified callback providing the
+      // found cached module
+      callback(mod);
+    }(mod));
+  }
+}
