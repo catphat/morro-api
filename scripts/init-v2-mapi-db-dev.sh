c3dda4ee04e6c0b19ce0876fc035117e07a72a57
catphat <kian@vedos.io>
2021-05-28 02:59:41 -0600

    update db name and fix init-v2-mapi-db-dev.sh

--- scripts/init-v2-mapi-db-dev.sh
+++ scripts/init-v2-mapi-db-dev.sh
@@ -3,22 +3,23 @@
 
 . ../.env
 
-PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "mapi_db_dev" -a -f '../src/db/sql/v02_deploy_bdo.sql'
-PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "mapi_db_dev" -a -f '../src/db/sql/v02_seed_bdo.sql'
-PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "mapi_db_dev" -c "\copy bdo.item(id, name) FROM 'seed/bdo_item_table.csv' CSV;"
-PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "mapi_db_dev" -c "\copy bdo.region(id, region_code) FROM 'seed/bdo_region_table.csv' CSV;"
-PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "mapi_db_dev" -c "\copy bdo.market_category(id, name) FROM 'seed/bdo_market_category_table.csv' CSV;"
+DB_NAME="mapi_db_dev_v2"
+PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "$DB_NAME" -a -f '../src/db/sql/v02_deploy_bdo.sql'
+PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "$DB_NAME" -a -f '../src/db/sql/v02_seed_bdo.sql'
+PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "$DB_NAME" -c "\copy bdo.item(id, name) FROM 'seed/bdo_item_table.csv' CSV;"
+PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "$DB_NAME" -c "\copy bdo.region(id, region_code) FROM 'seed/bdo_region_table.csv' CSV;"
+PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h $DB_HOST -d "$DB_NAME" -c "\copy bdo.market_category(id, name) FROM 'seed/bdo_market_category_table.csv' CSV;"
 
 IMPFILES=("$DB_SEED_DIR/*.csv")
-for i in "${IMPFILES[@]}";
+for i in ${IMPFILES[@]};
 do
       CSV=$(realpath "$i")
       echo "${CSV}"
       if [[ "$i" == *"-items.csv" ]];then
-          PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h "$DB_HOST" -d "mapi_db_dev" -c "\copy tmp_csv_items FROM '$CSV' DELIMITER '|' CSV HEADER NULL 'NULL';"
+          PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h "$DB_HOST" -d "$DB_NAME" -c "\copy tmp_csv_items FROM '$CSV' DELIMITER '|' CSV HEADER NULL 'NULL';"
       fi
  if [[ "$i" == *"-orders.csv" ]];then
-          PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h "$DB_HOST" -d "mapi_db_dev" -c "\copy tmp_csv_orders FROM '$CSV' DELIMITER '|' CSV HEADER NULL 'NULL';"
+          PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USERNAME" -h "$DB_HOST" -d "$DB_NAME" -c "\copy tmp_csv_orders FROM '$CSV' DELIMITER '|' CSV HEADER NULL 'NULL';"
       fi
 done
 
